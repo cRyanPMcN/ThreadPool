ff8301bf71c4ef887705467bbcc71b83b16274ee
Ryan M <ryanpmcn@gmail.com>
2022-03-21 21:55:27 -0400

    Changed ThreadPool to a facade pattern class.

--- Include/ThreadPool.hpp
+++ Include/ThreadPool.hpp
@@ -1,39 +1,43 @@
 #pragma once
 #include <cstddef>
+#include "ThreadPoolImpl.hpp"
+#include "ThreadPoolCPP.hpp"
+#include "ThreadPoolWin.hpp"
+#include "ThreadPoolWinVista.hpp"
 
 namespace Threading {
-	class ThreadPool {
+//#define THREADING_THREADPOOL_CUSTOM_DEFAULT ThreadPoolCPP
+#define THREADING_THREADPOOL_USE_STD
+	template <typename..._ArgsTy>
+	class ThreadPool : 
+		public
+#if defined(WIN32)
+#if _WIN32_WINNT > 0x0600
+		ThreadPoolWinVista<_ArgsTy...> 
+#else
+		ThreadPoolWin<_ArgsTy...>
+#endif
+#else
+		ThreadPoolCPP<_ArgsTy...>
+#endif
+						{
 	public:
-		struct Config {
-			std::size_t minimumThreads;
-			std::size_t maximumThreads;
-			std::size_t startingThreads;
+		using base_type =
+#if defined(WIN32)
+#if WINVER > 0x0600
+			ThreadPoolWinVista<_ArgsTy...>;
+#else
+			ThreadPoolWin<_ArgsTy...>;
+#endif
+#else
+			ThreadPoolCPP<_ArgsTy...>;
+#endif
 
-			Config() : minimumThreads(1), maximumThreads(16), startingThreads(1) {
-
-			}
-		};
-	protected:
-		const Config _config;
-		bool _run;
+		using Config = typename base_type::Config;
+		using work_type = typename base_type::work_type;
+		using work_container = typename base_type::work_container;
 	public:
-		ThreadPool(Config config = Config()) : _run(true), _config(config) {
-
-		}
-
-		~ThreadPool() {
-			Stop();
-		}
-
-		virtual void Stop() {
-			_run = false;
-		}
-
-		virtual void Wait() = 0;
-	};
-
-	class ThreadPoolWin {
-
+		using base_type::my_base;
 	};
 
 	class ThreadPoolWinVista {
