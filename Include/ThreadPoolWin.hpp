c3f3d089cd01b24f93835311433e46f07d180e65
Ryan M <ryanpmcn@gmail.com>
2022-04-25 17:04:03 -0400

    ThreadPoolWin implementation.

--- Include/ThreadPoolWin.hpp
+++ Include/ThreadPoolWin.hpp
@@ -1,204 +1,255 @@
 #pragma once
 
 #include "ThreadPoolImpl.hpp"
+#include <Windows.h>
 #include <vector>
 #include <queue>
-#include <thread>
 #include <tuple>
-#include <mutex>
-#include <functional>
 
 namespace Threading {
+	namespace DetailWin {
+		struct CriticalSection {
+			CRITICAL_SECTION _section;
+
+			CriticalSection() {
+				InitializeCriticalSection(&_section);
+			}
+
+			~CriticalSection() {
+				DeleteCriticalSection(&_section);
+			}
+
+			void Enter() {
+				EnterCriticalSection(&_section);
+			}
+
+			void Leave() {
+				LeaveCriticalSection(&_section);
+			}
+		};
+
+		struct SpinLock {
+		protected:
+			CriticalSection& _section;
+			unsigned long long _spinCount;
+		public:
+
+			SpinLock(CriticalSection& section) : _section(section), _spinCount(0) {
+				Lock();
+			}
+
+			~SpinLock() {
+				while (_spinCount) {
+					Unlock();
+				}
+			}
+
+			void Lock() {
+				_section.Enter();
+				_InterlockedIncrement(&_spinCount);
+			}
+
+			void Unlock() {
+				_section.Leave();
+				_InterlockedDecrement(&_spinCount);
+			}
+		};
+	}
+
 
 	template <class... _ArgsTy>
-	class ThreadPoolCPP : public ThreadPoolImpl<_ArgsTy...> {
+	class ThreadPoolWin : public ThreadPoolImpl<_ArgsTy...> {
 	public:
 		using base_type = ThreadPoolImpl<_ArgsTy...>;
 		using Config = typename base_type::Config;
-		using thread_type = std::thread;
+		using thread_type = struct _thrd {
+			HANDLE handle;
+			DWORD id;
+		};
 		using thread_container = std::vector<thread_type>;
+
+		template <typename _FuncTy>
+		struct ThreadData {
+			ThreadPoolWin& threadpool;
+			_FuncTy functor;
+
+			ThreadData(ThreadPoolWin& pool, _FuncTy func) : threadpool(pool), functor(func) {
+
+			}
+		};
+
+		template <typename _FuncTy, class _ObjTy>
+		struct MemberThreadData : ThreadData<_FuncTy> {
+			_ObjTy* object;
+
+			MemberThreadData(ThreadPoolWin& pool, _FuncTy func, _ObjTy* obj) : object(obj), ThreadData<_FuncTy>(pool, func) {
+
+			}
+		};
+
+		using lock_type = DetailWin::SpinLock;
+
 		using work_type = typename base_type::work_type;
 		using work_container = typename base_type::work_container;
 	protected:
-		std::queue<work_type> _works;
-		std::mutex _workMutex;
-		std::condition_variable _conditionVariable;
+		work_container _works;
+		CONDITION_VARIABLE _conditionVariable CONDITION_VARIABLE_INIT;
+		DetailWin::CriticalSection _criticalSection;
 		thread_container _threads;
-		//thread_type _watcherThread;
-		std::atomic_uint64_t _waitingThreads;
+		unsigned long long _waitingThreads;
+		// Required, gets cast to the correct type inside thread function
+		void* _threadData;
+		ThreadPoolWin(Config config) : _waitingThreads(0), base_type(config) {
+			InitializeConditionVariable(&_conditionVariable);
+		}
 	public:
 		template <typename _FuncTy>
-		ThreadPoolCPP(_FuncTy functor, Config config = Config()) : _waitingThreads(0), my_base(config) {
-			// _watcherThread(&ThreadPoolCPP::ThreadWrapper<_FuncTy>, this, functor),
+		ThreadPoolWin(_FuncTy functor, Config config = Config()) : ThreadPoolWin(config) {
+			_threadData = new ThreadData<_FuncTy>(*this, functor);
 			for (decltype(base_type::_config.startingThreads) i = 0; i < base_type::_config.startingThreads; ++i) {
-				_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<_FuncTy>, this, functor));
+				thread_type newThread;
+				newThread.handle = CreateThread(NULL, 0, &ThreadPoolWin::FunctionWrapper<_FuncTy>, _threadData, NULL, &newThread.id);
+				_threads.push_back(newThread);
 			}
 			Wait();
 		}
 
 		template <typename _RetTy>
-		ThreadPoolCPP(_RetTy(*functor)(_ArgsTy...), Config config = Config()) : _waitingThreads(0), my_base(config) {
-			// _watcherThread(&ThreadPoolCPP::ThreadWrapper<decltype(functor)>, this, functor), 
+		ThreadPoolWin(_RetTy(*functor)(_ArgsTy...), Config config = Config()) : ThreadPoolWin(config) {
+			_threadData = new ThreadData<decltype(functor)>(*this, functor);
 			for (decltype(base_type::_config.startingThreads) i = 0; i < base_type::_config.startingThreads; ++i) {
-				_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<decltype(functor)>, this, functor));
+				thread_type newThread;
+				newThread.handle = CreateThread(NULL, 0, &ThreadPoolWin::FunctionWrapper<decltype(functor)>, _threadData, NULL, &newThread.id);
+				_threads.push_back(newThread);
 			}
 			Wait();
 		}
 
 		template <typename _RetTy, class _ObjTy>
-		ThreadPoolCPP(_RetTy(_ObjTy::* functor)(_ArgsTy...), _ObjTy* obj, Config config = Config()) : _waitingThreads(0), my_base(config) {
-			// _watcherThread(&ThreadPoolCPP::ThreadWrapper<decltype(functor), _ObjTy>, this, functor, obj), 
+		ThreadPoolWin(_RetTy(_ObjTy::* functor)(_ArgsTy...), _ObjTy* obj, Config config = Config()) : ThreadPoolWin(config) {
+			_threadData = new MemberThreadData<decltype(functor), _ObjTy>(*this, functor, obj);
 			for (decltype(base_type::_config.startingThreads) i = 0; i < base_type::_config.startingThreads; ++i) {
-				_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<decltype(functor), _ObjTy*>, this, functor, obj));
+				thread_type newThread;
+				newThread.handle = CreateThread(NULL, 0, &ThreadPoolWin::FunctionWrapper<decltype(functor), _ObjTy>, _threadData, NULL, &newThread.id);
+				_threads.push_back(newThread);
 			}
 			Wait();
 		}
 
 		template <typename _RetTy, class _ObjTy>
-		ThreadPoolCPP(_RetTy(_ObjTy::* functor)(_ArgsTy...) const, _ObjTy const* obj, Config config = Config()) : _waitingThreads(0), my_base(config) {
-			// _watcherThread(&ThreadPoolCPP::ThreadWrapper<decltype(functor), _ObjTy const>, this, functor, obj), 
+		ThreadPoolWin(_RetTy(_ObjTy::* functor)(_ArgsTy...) const, _ObjTy const* obj, Config config = Config()) : ThreadPoolWin(config) {
+			_threadData = new MemberThreadData<decltype(functor), _ObjTy const>(*this, functor, obj);
 			for (decltype(base_type::_config.startingThreads) i = 0; i < base_type::_config.startingThreads; ++i) {
-				_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<decltype(functor), _ObjTy const*>, this, functor, obj));
+				thread_type newThread;
+				newThread.handle = CreateThread(NULL, 0, &ThreadPoolWin::FunctionWrapper<decltype(functor), _ObjTy const>, _threadData, NULL, &newThread.id);
+				_threads.push_back(newThread);
 			}
 			Wait();
 		}
 
-		~ThreadPoolCPP() {
+		~ThreadPoolWin() {
 			Stop();
-			for (std::thread& t : _threads) {
-				if (t.joinable()) {
-					t.join();
-				}
+			for (thread_type& t : _threads) {
+				WaitForSingleObject(t.handle, INFINITE);
 			}
-			//if (_watcherThread.joinable()) {
-			//	_watcherThread.join();
-			//}
+			delete _threadData;
 		}
+
+		using base_type::Push;
 
 		virtual void Push(work_type const& work) override {
-			std::unique_lock<std::mutex> lock(_workMutex);
+			lock_type lock(_criticalSection);
 			_works.push(work);
-			_conditionVariable.notify_one();
+			WakeOne();
 		}
 
 		virtual void Push(work_type const&& work) override {
-			std::unique_lock<std::mutex> lock(_workMutex);
+			lock_type lock(_criticalSection);
 			_works.push(work);
-			_conditionVariable.notify_one();
-		}
-
-		virtual void Push(_ArgsTy...args) {
-			Push(std::forward_as_tuple(args...));
+			WakeOne();
 		}
 
 		virtual void WakeOne() override {
-			_conditionVariable.notify_one();
-		}
-
-		virtual void Wake(std::size_t number) override {
-			while (number) {
-				WakeOne();
-				--number;
-			}
+			WakeConditionVariable(&_conditionVariable);
 		}
 
 		virtual void WakeAll() override {
-			_conditionVariable.notify_all();
+			WakeAllConditionVariable(&_conditionVariable);
 		}
 
 		virtual void Wait() override {
 			// Wait until all threads are waiting
-			while (_waitingThreads < _threads.size()) {
+			while (_waitingThreads < _threads.size() || !_works.empty()) {
 				std::this_thread::yield();
 			}
-		}
-
-		virtual void Stop() override {
-			base_type::_run = false;
-			WakeAll();
+			lock_type lock(_criticalSection);
 		}
 
 		template <typename _FuncTy>
-		void FunctionWrapper(_FuncTy functor) {
-			while (base_type::_run) {
+		static DWORD WINAPI FunctionWrapper(LPVOID threadData) {
+			ThreadData<_FuncTy>* data = (ThreadData<_FuncTy>*)threadData;
+			ThreadPoolWin& threadpool = data->threadpool;
+			work_container& works = threadpool._works;
+
+			while (threadpool._run) {
 				// Sleep thread
 				{
-					std::unique_lock<std::mutex> lock(_workMutex);
-					++_waitingThreads;
-					_conditionVariable.wait(lock);
-					--_waitingThreads;
+					lock_type lock(threadpool._criticalSection);
+					_InterlockedIncrement(&threadpool._waitingThreads);
+					SleepConditionVariableCS(&threadpool._conditionVariable, &lock._section._section, INFINITE);
+					_InterlockedDecrement(&threadpool._waitingThreads);
 				}
 
 				// Loop work execution
-				while (!_works.empty()) {
+				while (!works.empty()) {
 					// Acquire lock and ensure there is work to be done
-					std::unique_lock<std::mutex> lock(_workMutex);
-					if (_works.empty()) {
+					lock_type lock(threadpool._criticalSection);
+					if (works.empty()) {
 						break;
 					}
-					work_type work = _works.front();
-					_works.pop();
-					lock.unlock();
+					work_type work = works.front();
+					works.pop();
+					lock.Unlock();
 
-					_Execute(functor, work, std::make_index_sequence<sizeof...(_ArgsTy)>());
+					_Execute(data->functor, work, std::make_index_sequence<sizeof...(_ArgsTy)>());
 				}
 			}
+			
+			return EXIT_SUCCESS;
 		}
 
 		template <typename _FuncTy, class _ObjTy>
-		void FunctionWrapper(_FuncTy functor, _ObjTy obj) {
-			while (base_type::_run) {
+		static DWORD WINAPI FunctionWrapper(LPVOID threadData) {
+			MemberThreadData<_FuncTy, _ObjTy>* data = (MemberThreadData<_FuncTy, _ObjTy>*)threadData;
+			ThreadPoolWin& threadpool = data->threadpool;
+			work_container& works = threadpool._works;
+			_ObjTy* obj = data->object;
+
+			while (threadpool._run) {
 				// Sleep thread
 				{
-					std::unique_lock<std::mutex> lock(_workMutex);
-					++_waitingThreads;
-					_conditionVariable.wait(lock);
-					--_waitingThreads;
+					lock_type lock(threadpool._criticalSection);
+					_InterlockedIncrement(&threadpool._waitingThreads);
+					SleepConditionVariableCS(&threadpool._conditionVariable, &lock._section._section, INFINITE);
+					_InterlockedDecrement(&threadpool._waitingThreads);
 				}
 
 				// Loop work execution
-				while (!_works.empty()) {
+				while (!works.empty()) {
 					// Acquire lock and ensure there is work to be done
-					std::unique_lock<std::mutex> lock(_workMutex);
-					if (_works.empty()) {
+					lock_type lock(threadpool._criticalSection);
+					if (works.empty()) {
 						break;
 					}
-					work_type work = _works.front();
-					_works.pop();
-					lock.unlock();
+					work_type work = works.front();
+					works.pop();
+					lock.Unlock();
 
-					_Execute(functor, obj, work, std::make_index_sequence<sizeof...(_ArgsTy)>());
+					_Execute(data->functor, obj, work, std::make_index_sequence<sizeof...(_ArgsTy)>());
 				}
 			}
+
+			return EXIT_SUCCESS;
 		}
-
-		// Code has been removed as it does not function correctly, and I cannot fix it without the rest of the threadpool being functional
-		//template <typename _FuncTy>
-		//void ThreadWrapper(_FuncTy functor) {
-		//	while (_threads.size() < _config.startingThreads) {
-		//		_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<_FuncTy>, this, functor));
-		//	}
-		//	while (_run) {
-		//		if (_waitingThreads == 0 && _threads.size() < _config.maximumThreads) {
-		//			_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<_FuncTy>, this, functor));
-		//		}
-
-		//		// TODO: Remove threads, this will probably require a Thread wrapper object
-		//	}
-		//}
-
-		//template <typename _FuncTy, class _ObjTy>
-		//void ThreadWrapper(_FuncTy functor, _ObjTy* obj) {
-		//	while (_threads.size() < _config.startingThreads) {
-		//		_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<_FuncTy, _ObjTy>, this, functor, obj));
-		//	}
-		//	while (_run) {
-		//		if (_waitingThreads == 0 && _threads.size() < _config.maximumThreads) {
-		//			_threads.push_back(std::thread(&ThreadPoolCPP::FunctionWrapper<_FuncTy, _ObjTy>, this, functor, obj));
-		//		}
-
-		//		// TODO: Remove threads, this will probably require a Thread wrapper object
-		//	}
-		//}
 	};
 }
