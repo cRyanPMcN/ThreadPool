3e2c3001f3cd2ab174187240d502e356ad8a27fb
Ryan M <ryanpmcn@gmail.com>
2022-09-03 19:25:13 -0400

    Added(?) ThreadPoolBase::_Execute to get arguments out of the tuple.

--- Include/ThreadPoolBase.hpp
+++ Include/ThreadPoolBase.hpp
@@ -88,6 +88,16 @@ namespace Threading {
 		static inline void _Execute(_FuncTy functor, _ObjTy obj, work_type& work, std::index_sequence<_indexes...> indexSequence) {
 			std::invoke(std::move(functor), std::move(obj), std::get<_indexes>(work)...);
 		}
+
+		template <class _FuncTy>
+		static inline void _Execute(_FuncTy functor, work_type& work) {
+			_Execute(functor, work, std::make_index_sequence<sizeof...(_ArgsTy)>());
+		}
+
+		template <typename _FuncTy, class _ObjTy>
+		static inline void _Execute(_FuncTy functor, _ObjTy obj, work_type& work) {
+			_Execute(functor, obj, work, std::make_index_sequence<sizeof...(_ArgsTy)>());
+		}
 	};
 
 }
